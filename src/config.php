d4eb068fc09e5decde93fa9b0d7f3e9d47a399e9
Marco Dickert <marco@misterunknown.de>
2016-11-29 10:42:56 +0100

    misterunknown: cleaned format of config class

--- src/config.php
+++ src/config.php
@@ -15,28 +15,28 @@ class IFMConfig {
 	// 0 = no/not allowed;; 1 = yes/allowed;; default: no/forbidden;
 
 	// action controls
-	const upload = 1;				// allow uploads
+	const upload = 1;			// allow uploads
 	const remoteupload = 1;		// allow remote uploads using cURL
-	const delete = 1;				// allow deletions
-	const rename = 1;				// allow renamings
+	const delete = 1;			// allow deletions
+	const rename = 1;			// allow renamings
 	const edit = 1;				// allow editing
-	const chmod = 1;				// allow to change rights
+	const chmod = 1;			// allow to change rights
 	const extract = 1;			// allow extracting zip archives
-	const download = 1; 			// allow to download files and skripts (even php scripts!)
+	const download = 1; 		// allow to download files and skripts (even php scripts!)
 	const selfdownload = 1;		// allow to download this skript itself
-	const createdir = 1;			// allow to create directorys
-	const createfile = 1;			// allow to create files
+	const createdir = 1;		// allow to create directorys
+	const createfile = 1;		// allow to create files
 	const zipnload = 1;			// allow to zip and download directorys
 
 	// view controls
 	const multiselect = 1;		// implement multiselect of files and directories
 	const showlastmodified = 0;	// show the last modified date?
 	const showfilesize = 1;		// show filesize?
-	const showowner = 1;			// show file owner?
-	const showgroup = 1;			// show file group?
-	const showpath = 0; 			// show real path of directory (not only root)?
+	const showowner = 1;		// show file owner?
+	const showgroup = 1;		// show file group?
+	const showpath = 0; 		// show real path of directory (not only root)?
 	const showrights = 2; 		// show permissions 0 -> not; 1 -> octal, 2 -> human readable
-	const showhtdocs = 1;			// show .htaccess and .htpasswd
+	const showhtdocs = 1;		// show .htaccess and .htpasswd
 	const showhiddenfiles = 1;	// show files beginning with a dot (e.g. ".bashrc")
 
 	// general config
@@ -47,8 +47,8 @@ class IFMConfig {
 	// development tools
 	const ajaxrequest = 1;		// formular to perform an ajax request
 
-    static function getConstants() {
-        $oClass = new ReflectionClass(__CLASS__);
-        return $oClass->getConstants();
-    }
+	static function getConstants() {
+		$oClass = new ReflectionClass(__CLASS__);
+		return $oClass->getConstants();
+	}
 }
