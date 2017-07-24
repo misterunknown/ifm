191faa46787c728ee1d950fc885ac9d0a62e86f7
Marco Dickert <marco@misterunknown.de>
2017-07-24 15:01:11 +0200

    fix correct encoding/decoding of folder urls in the browser history

--- src/ifm.js
+++ src/ifm.js
@@ -228,7 +228,7 @@ function IFM( params ) {
 				self.currentDir = data.realpath;
 				self.refreshFileTable();
 				$( "#currentDir" ).val( self.currentDir );
-				if( config.pushState ) history.pushState( { dir: self.currentDir }, self.currentDir, "#"+self.currentDir );
+				if( config.pushState ) history.pushState( { dir: self.currentDir }, self.currentDir, "#"+encodeURIComponent( self.currentDir ) );
 			},
 			error: function() { self.showMessage( "General error occured: No or broken response", "e" ); }
 		});
@@ -1034,7 +1034,7 @@ function IFM( params ) {
 	 */
 	this.historyPopstateHandler = function(event) {
 		var dir = "";
-		if( event.state && event.state.dir ) dir = event.state.dir;
+		if( event.state && event.state.dir ) dir = decodeURIComponent( event.state.dir );
 		self.changeDirectory( dir, { pushState: false, absolute: true } );
 	};
 
