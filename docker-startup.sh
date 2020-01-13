f69a7eed4b162a5d4d55ade2d25c10b8110af96e
Marco Dickert <marco@misterunknown.de>
2020-01-13 16:53:03 +0100

    Completely new Dockerfile, add ability for different users (#121)
    Signed-off-by: Marco Dickert <marco@misterunknown.de>

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 set -e
 
-################################################################################
-# Adjust ifm configuration according to docker env vars.
-# Defaults should match those in ifm.php. See Dockerfile for details.
+if [ ! -z $IFM_DOCKER_UID ]; then
+	if [ -z $IFM_DOCKER_GID ]; then
+		export IFM_DOCKER_GID=$IFM_DOCKER_UID
+	fi
+	if getent passwd $IFM_DOCKER_UID >/dev/null 2>&1; then
+		deluser $(getent passwd $IFM_DOCKER_UID | sed "s/:.*//")
+	fi
+	if ! getent group $IFM_DOCKER_GID >/dev/null 2>&1; then
+		addgroup -g $IFM_DOCKER_GID -S ifm
+		REAL_GROUP=ifm
+	else
+		REAL_GROUP=$(getent group $IFM_DOCKER_GID | sed "s/:.*//")
+	fi
+	adduser -u $IFM_DOCKER_UID -HDG $REAL_GROUP ifm
+else
+	adduser -HD ifm
+fi
 
-################################################################################
-
-# Apache gets grumpy about PID files pre-existing
-rm -f /usr/local/apache2/logs/httpd.pid
-
-# Start up apache
-exec httpd -DFOREGROUND
-
+su ifm -c "php -S 0:80 -t /usr/local/share/webapps/ifm"
